8cc3c88fab027920245c9ba8c4abb48c3af7726a
jaafari-R <jaafariramadan@gmail.com>
2024-04-21 20:38:38 +0300

    Implemented Order class

--- src/main/java/com/att/tdp/bisbis10/order/Order.java
+++ src/main/java/com/att/tdp/bisbis10/order/Order.java
@@ -1,28 +1,59 @@
 package com.att.tdp.bisbis10.order;
 
+import java.util.List;
+
+import com.att.tdp.bisbis10.dish.Dish;
+import com.att.tdp.bisbis10.restaurant.Restaurant;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
 public class Order {
+    @Id
+    @SequenceGenerator(
+        name = "order_sequence",
+        sequenceName = "order_sequence",
+        allocationSize = 1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "order_sequeunce"
+    )
     private Long id;
-    private Long restaurantId;
+    @ManyToOne
+    private Restaurant restaurant;
+    @OneToMany
+    private List<Dish> orderItems;
 
     public Order() {
     }
 
-    public Order(Long id, Long restaurantId) {
+    public Order(Long id, Restaurant restaurant, List<Dish> orderItems) {
         this.id = id;
-        this.restaurantId = restaurantId;
+        this.restaurant = restaurant;
+        this.orderItems = orderItems;
     }
 
-    public Order(Long restaurantId) {
-        this.restaurantId = restaurantId;
+    public Order(Restaurant restaurant, List<Dish> orderItems) {
+        this.restaurant = restaurant;
+        this.orderItems = orderItems;
     }
-
 
 
     @Override
     public String toString() {
-        return "Order {" +
+        return "{" +
             " id='" + getId() + "'" +
-            ", restaurantId='" + getRestaurantId() + "'" +
+            ", restaurant='" + getRestaurant() + "'" +
+            ", orderItems='" + getOrderItems() + "'" +
             "}";
     }
 
@@ -34,12 +65,20 @@ public class Order {
         this.id = id;
     }
 
-    public Long getRestaurantId() {
-        return this.restaurantId;
+    public Long getRestaurant() {
+        return this.restaurant.getId();
     }
 
-    public void setRestaurantId(Long restaurantId) {
-        this.restaurantId = restaurantId;
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public List<Dish> getOrderItems() {
+        return this.orderItems;
+    }
+
+    public void setOrderItems(List<Dish> orderItems) {
+        this.orderItems = orderItems;
     }
 
 }
