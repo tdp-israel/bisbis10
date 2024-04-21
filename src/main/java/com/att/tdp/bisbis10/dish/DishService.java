318f14f57a3284ee27a5125ef4cb06ab947a2da4
jaafari-R <jaafariramadan@gmail.com>
2024-04-21 20:09:57 +0300

    Implemented Add dish

--- src/main/java/com/att/tdp/bisbis10/dish/DishService.java
+++ src/main/java/com/att/tdp/bisbis10/dish/DishService.java
@@ -1,13 +1,27 @@
 package com.att.tdp.bisbis10.dish;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.att.tdp.bisbis10.restaurant.Restaurant;
+import com.att.tdp.bisbis10.restaurant.RestaurantService;
+
+@Service
 public class DishService {
     DishRepository dishRepository;
+    RestaurantService restaurantService;
 
     @Autowired
-    public DishService(DishRepository dishRepository) {
+    public DishService(DishRepository dishRepository, RestaurantService restaurantService) {
         this.dishRepository = dishRepository;
+        this.restaurantService = restaurantService;
     }
 
+    public void addDish(Long restaurantId, Dish dish) {
+        Restaurant restaurant = restaurantService.getRestaurantById(restaurantId);
+        dish.setRestaurant(restaurant);
+        dishRepository.save(dish);
+    }
 }
