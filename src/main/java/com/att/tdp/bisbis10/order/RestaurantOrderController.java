b59d84814904174d59d79b447577bc12ae164fbe
jaafari-R <jaafariramadan@gmail.com>
2024-04-22 10:44:13 +0300

    working on Order and Order Item

--- src/main/java/com/att/tdp/bisbis10/order/RestaurantOrderController.java
+++ src/main/java/com/att/tdp/bisbis10/order/RestaurantOrderController.java
@@ -1,17 +1,26 @@
 package com.att.tdp.bisbis10.order;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/order")
 public class RestaurantOrderController {
-    RestaurantOrderService orderService;
+    RestaurantOrderService restaurantOrderService;
 
     @Autowired
-    public RestaurantOrderController(RestaurantOrderService orderService) {
-        this.orderService = orderService;
+    public RestaurantOrderController(RestaurantOrderService restaurantOrderService) {
+        this.restaurantOrderService = restaurantOrderService;
     }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addRestaurantOrder(@RequestBody OrderCreateRequest orderCreateRequest) {
+        restaurantOrderService.addRestaurantOrder(orderCreateRequest);
+    }
 }
--- src/main/java/com/att/tdp/bisbis10/order/RestaurantOrderService.java
+++ src/main/java/com/att/tdp/bisbis10/order/RestaurantOrderService.java
@@ -1,15 +1,36 @@
 package com.att.tdp.bisbis10.order;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.att.tdp.bisbis10.OrderItem.OrderItem;
+import com.att.tdp.bisbis10.dish.Dish;
+import com.att.tdp.bisbis10.restaurant.Restaurant;
+import com.att.tdp.bisbis10.restaurant.RestaurantService;
+
 @Service
 public class RestaurantOrderService {
-    RestaurantOrderRepository orderRepository;
+    RestaurantOrderRepository restaurantOrderRepository;
+    RestaurantService restaurantService;
 
     @Autowired
-    public RestaurantOrderService(RestaurantOrderRepository orderRepository) {
-        this.orderRepository = orderRepository;
+    public RestaurantOrderService(RestaurantOrderRepository restaurantOrderRepository, RestaurantService restaurantService) {
+        this.restaurantOrderRepository = restaurantOrderRepository;
+        this.restaurantService = restaurantService;
     }
     
+    public void addRestaurantOrder(OrderCreateRequest orderCreateRequest) {
+        Restaurant restaurant = restaurantService.getRestaurantById(orderCreateRequest.getRestaurantId());
+        List<Dish> orderItems = new ArrayList<>();
+        // TODO
+        // Add Dish does not exist error
+        for ( OrderItem orderItem : orderCreateRequest.getOrderItems()) {
+            
+        }
+        RestaurantOrder restaurantOrder = new RestaurantOrder(restaurant, orderItems);
+        restaurantOrderRepository.save(restaurantOrder);
+    }
 }
