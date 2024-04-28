d4f640008fc6eb6e1dd95f3bbdc032c813bac96d
jaafari-R <jaafariramadan@gmail.com>
2024-04-28 11:15:22 +0300

    Added Error handler for when json data received is of a wrong type

--- src/main/java/com/att/tdp/bisbis10/controlleradvice/ApplicationExceptionHandler.java
+++ src/main/java/com/att/tdp/bisbis10/controlleradvice/ApplicationExceptionHandler.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,16 +19,31 @@ public class ApplicationExceptionHandler {
     
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleInvalidArgument(MethodArgumentNotValidException e) {
-        List<String> errorsMessages = e.getBindingResult().getFieldErrors().stream()
+        List<String> errorMessages = e.getBindingResult().getFieldErrors().stream()
             .map(FieldError::getDefaultMessage)
             .collect(Collectors.toList());
 
         ErrorResponse error = new ErrorResponse(
             HttpStatus.BAD_REQUEST.value(),
-            errorsMessages
+            errorMessages
         );
 
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    // Handles Json Parsing Errors - when data is received with the wrong type
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidDatatypeArgument(HttpMessageNotReadableException e) {
+        List<String> errorMessages = List.of(
+            e.getMessage()
+        );
+
+        ErrorResponse error = new ErrorResponse(
+            HttpStatus.BAD_REQUEST.value(),
+            errorMessages
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
 }
