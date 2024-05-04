ea0e11a48ec2cc056aa7ba1f79b425f4cbe90900
YuvalOfek1 <yuvalofek97@gmail.com>
2024-05-04 15:39:51 +0300

    added validator in restaurantDTO

--- src/main/java/com/att/tdp/bisbis10/dto/RestaurantDTO.java
+++ src/main/java/com/att/tdp/bisbis10/dto/RestaurantDTO.java
@@ -1,11 +1,13 @@
 package com.att.tdp.bisbis10.dto;
 
 import jakarta.persistence.ElementCollection;
+import jakarta.validation.constraints.NotEmpty;
 
 import java.util.List;
 
 public record RestaurantDTO(
         Long id,
+        @NotEmpty
         String name,
         Float rating,
         Boolean isKosher,
